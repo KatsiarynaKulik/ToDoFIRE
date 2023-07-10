d33c8746c85629ae8b08a24e0a45dae3e276b7c1
Katsiaryna Kulik <125984123+KatsiarynaKulik@users.noreply.github.com>
2023-07-10 12:21:08 +0300

    Add Firebase

--- ToDoFIRE/Controllers/LoginViewController.swift
+++ ToDoFIRE/Controllers/LoginViewController.swift
@@ -17,7 +17,6 @@ class LoginViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
-    print("hello")
   }
 
   @IBAction func loginTapped(_ sender: UIButton) {
